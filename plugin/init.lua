32d7b34a4d936c5e6671b1376304f06b2a8182f6
patrick.joerg <patrick.joerg@gilgends.com>
2024-11-27 15:25:51 +0100

    Adding plugin to treesitter even though this is neovim specific

---
+++ plugin/init.lua
@@ -0,0 +1,25 @@
+-- Adding a new filetype to neovim
+vim.filetype.add({
+	extension = {
+		mac = "hicad",
+		MAC = "hicad",
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "hicad" },
+	callback = function(event)
+		vim.bo[event.buf].commentstring = "REM %s"
+	end,
+})
+
+require("nvim-treesitter.parsers").get_parser_configs().hicad = {
+	install_info = {
+		url = "https://github.com/petrisch/tree-sitter-hicad",
+		files = { "src/parser.c" },
+		branch = "main",
+		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+	},
+	filetype = "hicad", -- if filetype does not match the parser name
+}
