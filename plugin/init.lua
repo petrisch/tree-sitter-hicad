df2e1ec224d71bbdc92f6a58a5b4bfadc916ca88
patrick.joerg <patrick.joerg@gilgends.com>
2025-09-18 15:48:11 +0200

    WIP Trying to solve #1

--- plugin/init.lua
+++ plugin/init.lua
@@ -13,13 +13,14 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-require("nvim-treesitter.parsers").get_parser_configs().hicad = {
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.hicad = {
 	install_info = {
 		url = "https://github.com/petrisch/tree-sitter-hicad",
 		files = { "src/parser.c" },
 		branch = "main",
 		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-		requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
 	},
 	filetype = "hicad", -- if filetype does not match the parser name
 }
